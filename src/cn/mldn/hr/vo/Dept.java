9091437cae61179e909b77a04ec6b772dd2234c5
'mldn' <'mldnqa@163.com'>
2017-03-20 10:02:32 +0800

    增加了Dept类

---
+++ src/cn/mldn/hr/vo/Dept.java
@@ -0,0 +1,22 @@
+package cn.mldn.hr.vo;
+
+public class Dept {
+	private String dname;
+	private Integer deptno;
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDeptno(Integer deptno) {
+		this.deptno = deptno;
+	}
+
+	public Integer getDeptno() {
+		return deptno;
+	}
+}
